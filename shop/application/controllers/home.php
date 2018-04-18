424ae965c74954f29c993b9ae944c059ada0973f
mri-dev <pocokxtrame@gmail.com>
2018-04-18 21:12:51 +0200

    Save

---
+++ shop/application/controllers/home.php
@@ -0,0 +1,51 @@
+<?
+use PortalManager\News;
+use PortalManager\Template;
+
+class home extends Controller{
+		function __construct(){
+			parent::__construct();
+			parent::$pageTitle = '';
+
+			$this->out('homepage', true);
+			$this->out('bodyclass', 'homepage');
+
+			$news = new News( false, array( 'db' => $this->db ) );
+			$temp = new Template( VIEW .'hirek/template/' );
+
+			$arg = array(
+				'limit' => 30,
+				'page' 	=> 1,
+				'order' => array(
+					'by' => 'rand()'
+				)
+			);
+			$this->out( 'news', $news->getTree( $arg ) );
+			$this->out( 'template', $temp );
+
+			// SEO Információk
+			$SEO = null;
+			// Site info
+			$SEO .= $this->view->addMeta('description', $this->view->settings['about_us']);
+			$SEO .= $this->view->addMeta('keywords',$this->view->settings['page_keywords']);
+			$SEO .= $this->view->addMeta('revisit-after','3 days');
+
+			// FB info
+			$SEO .= $this->view->addOG('title', $this->view->settings['page_title'] . ' - '.$this->view->settings['page_description']);
+			$SEO .= $this->view->addOG('description', $this->view->settings['about_us']);
+			$SEO .= $this->view->addOG('type','website');
+			$SEO .= $this->view->addOG('url', CURRENT_URI );
+			$SEO .= $this->view->addOG('image', $this->view->settings['domain'].'/admin'.$this->view->settings['logo']);
+			$SEO .= $this->view->addOG('site_name', $this->view->settings['page_title']);
+			$this->view->SEOSERVICE = $SEO;
+		}
+
+		function __destruct(){
+			// RENDER OUTPUT
+				parent::bodyHead();					# HEADER
+				$this->view->render(__CLASS__);		# CONTENT
+				parent::__destruct();				# FOOTER
+		}
+	}
+
+?>
--- shop/application/views/v1.0/home/index.php
+++ shop/application/views/v1.0/home/index.php
@@ -1,14 +1,36 @@
 <div class="home">
 	<div class="pw">
-
 		<div class="grid-layout">
 			<div class="grid-row filter-sidebar">
 				<? $this->render('templates/sidebar'); ?>
 			</div>
 			<div class="grid-row inside-content">
-				<img src="https://www.cp.autoradiokeret.web-pro.hu/src/uploads/test.jpg" alt="">
-				<? //$this->render('templates/slideshow'); ?>
+				<? $this->render('templates/slideshow'); ?>
 			</div>
 		</div>
 	</div>
+	<div class="news">
+		<div class="pw">
+			<div class="articles">
+				<?
+				$step = 0;
+				while ( $this->news->walk() ) {
+					$step++;
+					$arg = $this->news->the_news();
+					$arg['date_format'] = $this->settings['date_format'];
+					echo $this->template->get( 'slide', $arg );
+				}?>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript">
+		$(function(){
+			$('.news .articles').slick({
+				infinite: true,
+			  slidesToShow: 3,
+			  slidesToScroll: 1,
+				dots: true
+			});
+		})
+	</script>
 </div>
