f1ffe9091b01e56eae6feffbfca0612d74900f3f
mri-dev <pocokxtrame@gmail.com>
2019-05-15 16:22:19 +0200

    Save

--- shop/application/views/v1.0/header.php
+++ shop/application/views/v1.0/header.php
@@ -32,6 +32,19 @@
   js.src = "//connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v2.3";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5cdc1ae8d07d7e0c6393bffb/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <header>
   <div class="top">
     <div class="pw">
